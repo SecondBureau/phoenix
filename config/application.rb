2e562363c38ee41d3ec0cb01e65272126146d945
Romain <romain@secondbureau.com>
2013-07-09 12:43:41 +0800

    init on compile true

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Phoenix
     config.assets.enabled = true
     
     # heroku labs:enable user-env-compile -a demo-phoenix
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
