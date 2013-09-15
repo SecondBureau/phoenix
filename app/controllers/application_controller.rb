4d00db09a1323b130703f2c22b2fd1c19e8aca0a
Gilles Crofils <gilles@secondbureau.com>
2013-09-15 16:59:31 +0800

    improved i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,39 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+
+  private
+  
+  def set_locale
+    if (session[:initialized].nil? || !session[:initialized])
+      locale = set_locale_from_param || set_locale_from_tld || set_locale_from_browser || Refinery::I18n.default_frontend_locale
+      logger.debug "* Locale set to '#{locale}'"
+      session[:initialized] = true
+      redirect_to refinery.url_for(:locale => locale) unless locale.eql?(Refinery::I18n.default_frontend_locale)
+    end
+  end
+  
+  def set_locale_from_param
+    logger.debug "* Param: '#{params[:locale]}'"
+    parsed_locale = params[:locale]
+    (!parsed_locale.nil? && !parsed_locale.eql?(I18n.default_locale) && Refinery::I18n.frontend_locales.include?(parsed_locale)) ? parsed_locale  : nil
+  end
+  
+  def set_locale_from_tld
+    logger.debug "* Host: '#{request.host}'"
+    parsed_locale = request.host.split('.').last.to_sym
+    Refinery::I18n.frontend_locales.include?(parsed_locale) ? parsed_locale  : nil
+  end
+  
+  def set_locale_from_browser
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    parsed_locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    begin
+      logger.debug "* Accept-Language: parsed_locale #{parsed_locale}"
+      Refinery::I18n.frontend_locales.include?(parsed_locale.to_sym) ? parsed_locale.to_sym  : nil
+    rescue
+    end
+  end
+  
 end
