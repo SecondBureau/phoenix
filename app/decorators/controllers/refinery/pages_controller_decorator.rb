0a7605d084a570ea2b1a20ac4bfb6d7a47e26645
Gilles Crofils <gilles@secondbureau.com>
2013-09-15 17:11:02 +0800

    bugfix caches_action

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,5 +1,5 @@
 Refinery::PagesController.class_eval do
   #caches_page :show
-  caches_action :home, :index, :if => Proc.new { |c| !local_request? && !refinery_user? } 
+  caches_action [:home, :index], :if => Proc.new { |c| !local_request? && !refinery_user? } 
   
 end
